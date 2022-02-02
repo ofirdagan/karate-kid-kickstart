ff61e885e6c4be9bb56d4c32ce4c60eccbb3d6d6
haimre <haimre@wix.com>
2022-02-02 18:06:39 +0200

    dynamic items

--- 1-Vanilla/index.js
+++ 1-Vanilla/index.js
@@ -13,22 +13,73 @@ function addItem(){
     }
     let subtitle = document.getElementById('subtitle-input').value
     let description = document.getElementById('description-input').value
-    console.log(title,subtitle,description)
     let list = document.getElementById('list')
-    // list.parentElement
-    let item = document.createElement('ol')
-    item.className = 'todo-item'
-    item.innerHTML = `    <input class='item-checkbox' type='checkbox'/>
-    <div class='todo-item-text'>
-        <div class='todo-item-title'>${title}</div>
-        <div class='todo-item-subtitle'>${subtitle}</div>
-        <div class='todo-item-description'>${description}</div>
-    </div>
-    <div class='todo-item-buttons'>
-        <button class='item-edit-button' onclick='goToEdit(this)'>edit</button>
-        <button class='item-delete-button' onclick='delete_item(this)'>delete</button>
-    </div>`
-    list.appendChild(item)
+    // ol todo-item
+    let itemElement = document.createElement('ol')
+    itemElement.className = 'todo-item'
+        
+        // input checkbox
+        let itemCheckboxElement = document.createElement('input')
+        itemCheckboxElement.type = 'checkbox'
+        itemCheckboxElement.className = 'item-checkbox'
+        // div todo-item-text
+        let itemTextElement = document.createElement('div')
+        itemTextElement.className = 'todo-item-text'
+            // item title
+            let titleElement = document.createElement('div')
+            titleElement.className = 'todo-item-title'
+            titleElement.innerText = title
+            // item subtitle
+            let subtitleElement = document.createElement('div')
+            subtitleElement.innerText = subtitle
+            subtitleElement.className = 'todo-item-subtitle'
+            // item description
+            let descriptionElement = document.createElement('div')
+            descriptionElement.innerText = description
+            descriptionElement.className = 'todo-item-description'
+        
+            // div todo-item-buttons
+        let itemButtonsElement = document.createElement('div')
+        itemButtonsElement.className = 'todo-item-buttons'
+            // button edit
+            let editButtonElement = document.createElement('button')
+            editButtonElement.innerText = 'edit'
+            editButtonElement.className = 'item-edit-button'
+            editButtonElement.setAttribute('onclick','goToEdit(this)')
+            // button edit
+            let deleteButtonElement = document.createElement('button')
+            deleteButtonElement.innerText = 'delete'
+            deleteButtonElement.className = 'item-delete-button'
+            deleteButtonElement.setAttribute('onclick','delete_item(this)')
+
+    // append the text to parent
+    itemTextElement.appendChild(titleElement)
+    itemTextElement.appendChild(subtitleElement)
+    itemTextElement.appendChild(descriptionElement)
+    // append the buttons to parent
+    itemButtonsElement.appendChild(editButtonElement)
+    itemButtonsElement.appendChild(deleteButtonElement)
+    //append checkbox, text and buttons to list-item
+    itemElement.appendChild(itemCheckboxElement)
+    itemElement.appendChild(itemTextElement)
+    itemElement.appendChild(itemButtonsElement)
+
+    // the itemElement should look like this
+    // <ol class='todo-item'>
+    //    <input class='item-checkbox' type='checkbox'/>
+    //    <div class='todo-item-text'>
+    //        <div class='todo-item-title'>Go for a walk with the dog</div>
+    //        <div class='todo-item-subtitle'>very fun</div>
+    //        <div class='todo-item-description'>walk around the block 3 times, bring a doggy bag</div>
+    //    </div>
+    //    <div class='todo-item-buttons'>
+    //        <button class='item-edit-button' onclick='goToEdit(this)'>edit</button>
+    //        <button class='item-delete-button' onclick='delete_item(this)'>delete</button>
+    //    </div> 
+    // </ol>
+
+    // finally append item to the list
+    list.appendChild(itemElement)
     document.getElementById('title-input').value = ''
     document.getElementById('subtitle-input').value = ''
     document.getElementById('description-input').value = ''
@@ -44,6 +95,10 @@ function goToEdit(caller){
     document.getElementById('menu-button').innerText = 'add changes'
 }
 function delete_item(caller){
+    if(editMode){
+        console.log('cant delete while in edit mode')
+        return
+    }
     let item = caller.parentElement.parentElement
     console.log(item)
     let list = document.getElementById('list')
@@ -80,4 +135,10 @@ function menu_button(caller){
         document.getElementById('menu-button').innerText = 'add item'
         document.getElementById('cancel-button').hidden = true
     }
+}
+function add_button(caller){
+    document.getElementById('title-input').value = ''
+    document.getElementById('subtitle-input').value = ''
+    document.getElementById('description-input').value = ''
+    document.getElementById('title-input').focus()
 }
