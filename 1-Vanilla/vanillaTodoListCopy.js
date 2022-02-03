c68f899bb5c7b1abca9c447128d2ff1a38ba18b5
Yuval Levy <yuvallevy@wix.com>
2022-02-03 16:02:35 +0200

    enabaling edit button work

--- 1-Vanilla/vanillaTodoListCopy.js
+++ 1-Vanilla/vanillaTodoListCopy.js
@@ -4,35 +4,32 @@
 
 
 const createButton = (type, todoObj) => {
-    debugger;
     let button = document.createElement('button');
     const buttonClassName = `${type}-button`
     button.classList.add(buttonClassName)
-    button.appendChild(createIcon(type))
+    button.appendChild(createIcon(type, todoObj.id))
     // button.addEventListener('click', handleButtonClick)
     button.onclick = function(){handleButtonClick(type, todoObj.id)}
     return button
 } 
-const createIcon = (type)  => {
+const createIcon = (type, id)  => {
     debugger;
     let icon = document.createElement('i');
     let iconClassName = ""
-    
+    let iconId = ``
     if (type === 'delete'){
         iconClassName = "fas fa-trash";
+        iconId = `icon-delete-${id}`
     }
     else if (type === 'edit') {
         iconClassName = "fas fa-edit";
-
+        iconId = `icon-edit-${id}`
     }else{ 
         console.log('text');;
     }
-    icon.innerHTML = `<i class="${iconClassName}"></i>`
+    debugger
+    icon.innerHTML = `<i class="${iconClassName}" id="${iconId}"></i>`
     return icon
-}
-
-const createToDo = () => {
-
 }
 
 
@@ -41,8 +38,8 @@ function handleButtonClick(type, todoId) {
     debugger
     if (type === 'delete'){
         handleDeleteTodo(todoId)
-    }else if (type === 'edit-button'){
-        handleEditTodo()
+    }else if ('edit'){
+        handleEditTodo(todoId)
     }else {
         console.log('text');;
 
@@ -54,7 +51,7 @@ function handleSubmitTodo(e) {
     e.preventDefault();
 
     let input = document.getElementById('new-task-input');
-    if (input.value != '') {
+    if (input.value.trim() != '') {
         // add the todo
         addTodo(input.value)
     }else {
@@ -73,6 +70,16 @@ function handleDeleteTodo(todoId) {
 
 function handleEditTodo(todoId){
     let li = document.getElementById(`todo-list-item-${todoId}`)
+    debugger;
+    let todoItem = document.getElementById(`todo-item-${todoId}`)
+    if (todoItem.disabled) {
+        todoItem.disabled = false
+        let todoIcon = document.getElementById(`icon-edit-${todoId}`)
+    } else {
+        let todoItem = document.getElementById(`todo-item-${todoId}`)
+        // const todoItemText =  todoItem.value
+        todoItem.disabled = true
+    }
 }
 
 
@@ -90,11 +97,15 @@ function addTodo(todoText) {
 
     let ul = document.getElementById('todo-list')
     let li = document.createElement('li')
-    let todoDiv = document.createElement('div');
+    let todoDiv = document.createElement('input');
     todoDiv.classList.add('todo-item');
-    todoDiv.innerHTML = todoText
+    todoDiv.disabled = true
+    todoDiv.id = (`todo-item-${todoId}`)
+    // todoDiv.innerHTML = todoText
+    todoDiv.value = todoText
     const deleteButton = createButton('delete', todoObj)
     const editButton = createButton('edit', todoObj)
+    
     // add content to li
     li.appendChild(todoDiv)
     li.appendChild(deleteButton)
