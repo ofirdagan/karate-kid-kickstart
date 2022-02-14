2ee329aa55a63b5babe3f2c169fec61bece72f68
ItayGershman <48354712+ItayGershman@users.noreply.github.com>
2022-02-14 15:38:48 +0200

    Jss (#5)
    * JSS init
    
    * converting css to jss
    
    * added bundle analyzer into the app
    
    * extract jss imports and setup to style-config file
    
    * fix style files

---
+++ 3-JSS/js-styles/colors.js
@@ -0,0 +1,7 @@
+export const colors = {
+  blue: "#1e90ff",
+  white: "#ffffff",
+  redDanger: "#b63423",
+  green: "#30b44c",
+  black: "#000000",
+};
---
+++ 3-JSS/utils/localstorage.js
@@ -0,0 +1,23 @@
+export class LocalStorage {
+  getList() {
+    return JSON.parse(localStorage.getItem("todoList"));
+  }
+  #saveList(list) {
+    if (!localStorage.getItem("todoList"))
+      localStorage.setItem("todoList", JSON.stringify(list));
+  }
+  addTodo(item) {
+    this.#saveList([...this.getList(), item]);
+  }
+  editTodo(newTodo, id) {
+    const newList = this.getList().map((todo) => {
+      if (todo.id === id) return newTodo;
+      else return todo;
+    });
+    this.#saveList(newList);
+  }
+  removeTodo(id) {
+    const newList = this.getList().filter((todo) => todo.id === id);
+    this.#saveList(newList);
+  }
+}
