6bdf40be3f205d5c9169d0b046ae104d9639001d
Sean Ocheri <seano@wix.com>
2022-02-20 16:46:17 +0200

    Added MongoDB with default user

--- 5-Heroku/server/todo_service.js
+++ 5-Heroku/server/todo_service.js
@@ -1,26 +1,82 @@
-const items = {};
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const MONGO_URI =
+  "mongodb+srv://mongo:1J6rVJPjdrskmtS8@mychecklistcluster.z23uf.mongodb.net/MyChecklist";
+
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("MobgoDB is connected!");
+});
+
+const TodoSchema = new Schema({
+  user_id: String,
+  id: String,
+  text: String,
+  check: Boolean,
+});
+
+const Todo = mongoose.model("Todo", TodoSchema);
+
+function dbUpdateCallback(res, successStatus) {
+  return function (error, doc) {
+    if (error) {
+      console.log(error);
+      res.status(500).send("Database error!");
+    } else {
+      res.sendStatus(201);
+    }
+  };
+}
 
 module.exports.getItems = (req, res) => {
-  res.json(items);
+  Todo.find({ user_id: 0 }, function (err, docs) {
+    if (err) {
+      console.log(err);
+      res.sendStatus(500);
+    } else {
+      var items = {};
+
+      docs.forEach(function (doc) {
+        items[doc.id] = {
+          text: doc.text,
+          check: doc.check,
+        };
+      });
+
+      res.send(items);
+    }
+  });
 };
 
 module.exports.createItem = (req, res) => {
-  items[req.body.id] = {
+  const todo = new Todo({
+    user_id: 0,
+    id: req.body.id,
     text: req.body.text,
     check: req.body.check,
-  };
-  res.sendStatus(201);
+  });
+
+  todo.save(dbUpdateCallback(res, 201));
 };
 
 module.exports.editItem = (req, res) => {
-  items[req.params.id] = {
+  const query = { user_id: 0, id: req.params.id };
+  const item = {
+    user_id: 0,
+    id: req.params.id,
     text: req.body.text,
     check: req.body.check,
   };
-  res.sendStatus(200);
+
+  Todo.findOneAndUpdate(query, item, dbUpdateCallback(res, 200));
 };
 
 module.exports.deleteItem = (req, res) => {
-  delete items[req.params.id];
-  res.sendStatus(200);
+  const query = { user_id: 0, id: req.params.id };
+  Todo.deleteOne(query, dbUpdateCallback(res, 200));
 };
